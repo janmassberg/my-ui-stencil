892ceba02018f95b3062b30f4095d75aabb5ee1a
Jan Massberg <jan.massberg@odt.net>
2021-10-24 21:19:48 +0200

    test(button): update button e2e tests

--- src/components/button/button.e2e.ts
+++ src/components/button/button.e2e.ts
@@ -1,10 +1,13 @@
 import { newE2EPage } from "@stencil/core/testing";
 
-describe("my-button", () => {
-  it("renders", async () => {
+describe("my-ui-button", () => {
+  it("should have variant classNames and render button with aria-label attribute", async () => {
     const page = await newE2EPage();
-    await page.setContent('<my-button label="Label">Content</my-button>');
-    const el = await page.find("my-button");
+    await page.setContent(
+      '<my-ui-button label="Label" kind="primary">Content</my-ui-button>',
+    );
+    const el = await page.find("my-ui-button");
+    await page.waitForChanges();
     expect(el).toHaveClass("hydrated");
     expect(el).toMatchSnapshot();
   });
