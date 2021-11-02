3aea9f4f3a2c3761c7707e027d8686e6ad31efa9
Jan Massberg <jan.massberg@odt.net>
2021-11-02 14:52:25 +0100

    feat(icon): add icon svgs: app, grid, list

--- src/components/icon/types.ts
+++ src/components/icon/types.ts
@@ -1,8 +1,11 @@
 export enum IconName {
+  APPS = "apps",
   CARET_DOWN = "caret-down",
   CARET_LEFT = "caret-left",
   CARET_RIGHT = "caret-right",
   CARET_UP = "caret-up",
+  GRID = "grid",
+  LIST = "list",
 }
 
 export type IconSize = "s" | "m" | "l";
