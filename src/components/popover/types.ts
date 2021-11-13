891bd97036c1d0b60d6dd70fb614e382f08bb893
Jan Massberg <jan.massberg@odt.net>
2021-11-13 20:54:17 +0100

    feat(popover): add popover position and alignment enums to types.ts

--- src/components/popover/types.ts
+++ src/components/popover/types.ts
@@ -1,32 +1,52 @@
 export type PopoverPlacementName =
-    | "top"
-    | "top-start"
-    | "top-end"
-    | "bottom"
-    | "bottom-start"
-    | "bottom-end"
-    | "left"
-    | "left-start"
-    | "left-end"
-    | "right"
-    | "right-start"
-    | "right-end";
+  | "top"
+  | "top-start"
+  | "top-end"
+  | "bottom"
+  | "bottom-start"
+  | "bottom-end"
+  | "left"
+  | "left-start"
+  | "left-end"
+  | "right"
+  | "right-start"
+  | "right-end";
 
-export type PopoverPlacementPosition = "top" | "bottom" | "left" | "right";
+export enum PopoverPositions {
+  BOTTOM = "bottom",
+  LEFT = "left",
+  RIGHT = "right",
+  TOP = "top",
+}
 
-export type PopoverPlacementAlignment = "start" | "end" | "center";
+export type PopoverPlacementPosition =
+  | PopoverPositions.BOTTOM
+  | PopoverPositions.LEFT
+  | PopoverPositions.RIGHT
+  | PopoverPositions.TOP;
+
+export enum PopoverAlignments {
+  CENTER = "center",
+  END = "end",
+  START = "start",
+}
+
+export type PopoverPlacementAlignment =
+  | PopoverAlignments.CENTER
+  | PopoverAlignments.END
+  | PopoverAlignments.START;
 
 export interface PopoverDimensions {
-    left: number;
-    top: number;
-    width: number;
-    height: number;
-    placement: PopoverPlacementName;
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+  placement: PopoverPlacementName;
 }
 
 export interface PopoverOptions {
-    safeMargin: number;
-    viewportWidth: number;
-    viewportHeight: number;
-    offsetFromTarget: number;
+  safeMargin: number;
+  viewportWidth: number;
+  viewportHeight: number;
+  offsetFromTarget: number;
 }
